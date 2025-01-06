ec4c9ed58f4c8bfb0b0dcf7ef30e62dbe28850cf
Matt Himrod <matt@matthimrod.com>
2025-01-05 22:07:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Kasa Utility
+
+A utility that uses two Kasa Smart Plugs with current monitoring to monitor one device for usage and use it to trigger a different device to turn off.
+
+My use case is to automatically turn off a space heater when my husband uses our treadmill so that the heater doesn't cause the circuit breaker to trip.
+
+## Usage
+
+Environment Variables:
+
+* KASA_USERNAME - the username to use for authentication
+* KASA_PASSWORD - the password to use for authentication
+* KASA_THRESHOLD - the threshold in amps
+* KASA_LOAD - the hostname or IP address of the smart device to monitor
+* KASA_SWITCH - the hostname or IP address of the smart device to turn off
+
+Crontab:
+
+The script will check the devices and change states once and then exit. The intended use case is a crontab entry that runs the script every minute during the hours when treadmill useage is likey. (Otherwise, it can be run every minute perpetually.)
+
+Example:
+
+```crontab
+ * 8-23 * * *   . /root/kasa_utility/.venv/bin/activate && KASA_USERNAME=user@mail.com KASA_PASSWORD=my_password KASA_THRESHOLD=2.0 KASA_LOAD=192.168.1.2 KASA_SWITCH=192.168.1.3 python /root/kasa_utility/kasa_utility.py
+```
+
+This will check every minute from 8:00 AM to 11:00 PM.
---
+++ kasa_utility.py
@@ -0,0 +1,65 @@
+import asyncio
+import logging
+import os
+import sys
+from typing_extensions import TypedDict, Unpack
+
+from kasa import Discover
+
+logging.basicConfig(handlers=[logging.StreamHandler(sys.stdout)],
+                    encoding='utf-8',
+                    level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
+
+
+class _MainKwargs(TypedDict):
+    username: str
+    password: str
+    threshold: float
+    load_host: str
+    switch_host: str
+
+
+async def main(**kwargs: Unpack[_MainKwargs]):
+    """
+    Main Function - Turns off the heater if the threshold is exceeded.
+    """
+    username = kwargs['username']
+    password = kwargs['password']
+    threshold = kwargs['threshold']
+
+    load_host = await Discover.discover_single(host=kwargs['load_host'], username=username, password=password)
+    switch_host = await Discover.discover_single(host=kwargs['switch_host'], username=username, password=password)
+
+    await switch_host.update()
+    switch_state = switch_host.state_information['State']
+    switch_current = switch_host.state_information['Current consumption']
+
+    logging.info('Switch device powered on: %s, current is: %s A', switch_state, switch_current)
+
+    await load_host.update()
+    load_state = load_host.state_information['State']
+    load_current = load_host.state_information['Current consumption']
+
+    logging.info('Load device powered on: %s, current is: %s A', load_state, load_current)
+
+    if load_current > threshold:
+        logging.info('Load device is drawing more than %s A. Turning off the switch device.', 
+                     threshold)
+        await switch_host.turn_off()
+    elif not switch_state and load_current < threshold:
+        logging.info('Load device is drawing less than %s A. Turning on the switch device.', 
+                     threshold)
+        await switch_host.turn_on()
+
+    await load_host.disconnect()
+    await switch_host.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(username=os.environ['KASA_USERNAME'], 
+                     password=os.environ['KASA_PASSWORD'], 
+                     threshold=float(os.environ['KASA_THRESHOLD']),
+                     load_host=os.environ['KASA_LOAD'],
+                     switch_host=os.environ['KASA_SWITCH']))
